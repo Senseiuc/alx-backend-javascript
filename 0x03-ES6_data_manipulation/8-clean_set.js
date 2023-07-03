51606ed9c06c938f1ab14a760aa5c8beac528d67
senseiuc <dhinhojboy@gmail.com>
2023-07-03 15:47:34 +0000

    clean set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,13 @@
+export default function cleanSet(set, startString) {
+  const parts = [];
+  for (const value of set.values()) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      const valueSubStr = value.substring(startString.length);
+
+      if (valueSubStr && valueSubStr !== value) {
+        parts.push(valueSubStr);
+      }
+    }
+  }
+  return parts.join('-');
+}
