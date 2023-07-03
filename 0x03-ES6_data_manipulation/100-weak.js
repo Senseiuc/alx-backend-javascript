423c5918b4a510a89f172ec038844b22823a9525
senseiuc <dhinhojboy@gmail.com>
2023-07-03 16:02:49 +0000

    weak

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,17 @@
+/**
+ * A weak map.
+ */
+export const weakMap = new WeakMap();
+
+/**
+ * Tracks the number of calls made to an API's endpoint
+ */
+export function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  if (weakMap.get(endpoint) >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+}
